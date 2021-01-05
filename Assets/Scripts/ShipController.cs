44d40ac785227abbac5ef26166b7c223e909af39
Tumppi <kuusisto.tuomo@gmail.com>
2021-01-05 21:03:54 +0200

    Added third control mode (rigidbody rotation) and switch button to UI

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipController : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class ShipController : MonoBehaviour
     [Tooltip("EXPERIMENTAL: for non-classic control that handles single thruster usage separately from L+R thruster combo")]
     public float sideThrusterToMainThrusterRatio = 1;
     public float maxVelocity = 10f;
-    [Tooltip("EXPERIMENTAL: Non-classic control handles single thruster usage separately from L+R thruster combo")]
-    public bool classicControls = true;
+    public enum ControlType { Original, BoostedThrusters, RigidBodyRotation }
+    [Tooltip("EXPERIMENTAL: Non-classic control handles single thruster usage separately from L+R thruster combo. RigidBody mode rotates rigidbody instead of adding force to thrusters")]
+    public ControlType currentControlType;
     public float maxAngularVelocity = 10f;
     public Transform leftEngine;
     public Transform rightEngine;
     public Rigidbody2D RigidBody { get; private set; }
+    public Text controlText; // Debug text
 
     [SerializeField]
     bool _leftEngineOn;
@@ -35,6 +38,24 @@ public class ShipController : MonoBehaviour
         {
             Reset();
         }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            ToggleLeftEngine(true);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            ToggleLeftEngine(false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            ToggleRightEngine(true);
+        }
+        else if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            ToggleRightEngine(false);
+        }
     }
 
     public void ToggleLeftEngine(bool newState)
@@ -55,80 +76,103 @@ public class ShipController : MonoBehaviour
         RigidBody.angularVelocity = 0;
     }
 
+    public void SwitchControlScheme()
+    {
+        switch(currentControlType)
+        {
+            case ControlType.Original:
+                currentControlType = ControlType.BoostedThrusters;
+                break;
+            case ControlType.BoostedThrusters:
+                currentControlType = ControlType.RigidBodyRotation;
+                break;
+            case ControlType.RigidBodyRotation:
+                currentControlType = ControlType.Original;
+                break;
+        }
+
+        controlText.text = currentControlType.ToString();
+    }
+
     IEnumerator EngineRoutine()
     {
+        YieldInstruction yi = new WaitForFixedUpdate();
         while(true)
         {
-            if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            switch(currentControlType)
             {
-                ToggleLeftEngine(true);
-            }
-            else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
-            {
-                ToggleLeftEngine(false);
-            }
-
-            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-            {
-                ToggleRightEngine(true);
-            }
-            else if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
-            {
-                ToggleRightEngine(false);
-            }
-
-            if(classicControls)
-            {
-                if(_leftEngineOn)
-                {
-                    if(RigidBody.angularVelocity > -maxAngularVelocity)
+                case ControlType.Original:
+                    if(_leftEngineOn)
                     {
-                        RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
-                        Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce, Color.red);
+                        if(RigidBody.angularVelocity > -maxAngularVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
+                            Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce, Color.red);
+                        }
                     }
-                }
 
-                if(_rightEngineOn)
-                {
-                    if(RigidBody.angularVelocity < maxAngularVelocity)
+                    if(_rightEngineOn)
                     {
-                        RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
-                        Debug.DrawLine(rightEngine.position, rightEngine.position + rightEngine.up * engineForce, Color.green);
+                        if(RigidBody.angularVelocity < maxAngularVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
+                            Debug.DrawLine(rightEngine.position, rightEngine.position + rightEngine.up * engineForce, Color.green);
+                        }
                     }
-                }
+                    break;
+                case ControlType.BoostedThrusters:
+                    if(_leftEngineOn && !_rightEngineOn)
+                    {
+                        if(RigidBody.angularVelocity > -maxAngularVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
+                            Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce * sideThrusterToMainThrusterRatio, Color.red);
+                        }
+                    }
+                    else if(_rightEngineOn && !_leftEngineOn)
+                    {
+                        if(RigidBody.angularVelocity < maxAngularVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
+                            Debug.DrawLine(rightEngine.position, rightEngine.position + rightEngine.up * engineForce * sideThrusterToMainThrusterRatio, Color.green);
+                        }
+                    }
+                    else if(_leftEngineOn && _rightEngineOn)
+                    {
+                        if(RigidBody.velocity.sqrMagnitude < maxVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
+                            RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
+                            Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce, Color.red);
+                        }
+                    }
+                    break;
+                case ControlType.RigidBodyRotation:
+                    if(_leftEngineOn && !_rightEngineOn)
+                    {
+                        RigidBody.angularVelocity = 0;
+                        RigidBody.MoveRotation(RigidBody.rotation - maxAngularVelocity * Time.deltaTime);
+                    }
+
+                    if(_rightEngineOn && !_leftEngineOn)
+                    {
+                        RigidBody.angularVelocity = 0;
+                        RigidBody.MoveRotation(RigidBody.rotation + maxAngularVelocity * Time.deltaTime);
+                    }
+
+                    if(_leftEngineOn && _rightEngineOn)
+                    {
+                        if(RigidBody.velocity.sqrMagnitude < maxVelocity)
+                        {
+                            RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
+                            RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
+                            Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce, Color.red);
+                        }
+                    }
+                    break;
             }
-            else //Non-classic controls. L+R combo press produces less combined thrust, making single-L or single-R button press rotate ship faster (but more difficult to handle)
-            {
-                if(_leftEngineOn && !_rightEngineOn)
-                {
-                    if(RigidBody.angularVelocity > -maxAngularVelocity)
-                    {
-                        RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
-                        Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce * sideThrusterToMainThrusterRatio, Color.red);
-                    }
-                }
 
-                if(_rightEngineOn && !_leftEngineOn)
-                {
-                    if(RigidBody.angularVelocity < maxAngularVelocity)
-                    {
-                        RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
-                        Debug.DrawLine(rightEngine.position, rightEngine.position + rightEngine.up * engineForce * sideThrusterToMainThrusterRatio, Color.green);
-                    }
-                }
-
-                if(_leftEngineOn && _rightEngineOn)
-                {
-                    if(RigidBody.velocity.sqrMagnitude < maxVelocity)
-                    {
-                        RigidBody.AddForceAtPosition(leftEngine.up * engineForce, leftEngine.position);
-                        RigidBody.AddForceAtPosition(rightEngine.up * engineForce, rightEngine.position);
-                        Debug.DrawLine(leftEngine.position, leftEngine.position + leftEngine.up * engineForce, Color.red);
-                    }
-                }
-            }
-
-            yield return null;
+            yield return yi;
         }
     }
 
